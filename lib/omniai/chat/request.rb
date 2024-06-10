84fa824344261b36c91a7874073913c64ad9ea81
Kevin Sylvestre <kevin@ksylvest.com>
2024-06-10 12:12:14 -0700

    Setup

---
+++ lib/omniai/chat/request.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+
+module OmniAI
+  class Chat
+    # An abstract class that provides a consistent interface for processing chat requests.
+    #
+    # Usage:
+    #
+    #    class OmniAI::OpenAI::ChatGPT::Request < OmniAI::Chat::Request
+    #      module Model
+    #        CHAT = "davinci"
+    #      end
+    #      def completion(messages, model:, temperature: 0.0, format: :text)
+    #      end
+    #    end
+    #
+    # Once defined, the subclass can be used to interface with the vendor's chat API as follows:
+    #
+    #    client.chat.completion(messages, model: "...", temperature: 0.0, format: :text)
+    class Request
+      # @param client [OmniAI::Client] the client
+      # @param messages [String] required
+      # @param model [String] required
+      # @param temperature [Float, nil] optional
+      # @param stream [Proc, nil] optional
+      # @param format [Symbol, nil] optional - :json
+      def initialize(client:, messages:, model:, temperature: nil, stream: nil, format: nil)
+        @client = client
+        @messages = messages
+        @model = model
+        @temperature = temperature
+        @stream = stream
+        @format = format
+      end
+
+      # @raise [ExecutionError]
+      def process!
+        response = request!
+        raise HTTPError, response unless response.status.ok?
+
+        parse!(response:)
+      end
+
+      protected
+
+      # @param response [HTTP::Response]
+      # @return [OmniAI::Chat::Completion]
+      def parse!(response:)
+        if @stream
+          stream!(response:)
+        else
+          OmniAI::Chat::Completion.new(data: response.parse)
+        end
+      end
+
+      # @param response [HTTP::Response]
+      # @return [OmniAI::Chat::Chunk]
+      def stream!(response:)
+        parser = EventStreamParser::Parser.new
+
+        response.body.each do |chunk|
+          parser.feed(chunk) do |_, data|
+            break if data.eql?('[DONE]')
+
+            chunk = OmniAI::Chat::Chunk.new(data: JSON.parse(data))
+            @stream.call(chunk)
+          end
+        end
+      end
+
+      # @return [Hash]
+      def payload
+        raise NotImplementedError, "#{self.class.name}#payload undefined"
+      end
+
+      # @return [String]
+      def path
+        raise NotImplementedError, "#{self.class.name}#path undefined"
+      end
+
+      # @return [HTTP::Response]
+      def request!
+        @client
+          .connection
+          .accept(:json)
+          .post(path, json: payload)
+      end
+    end
+  end
+end
---
+++ lib/omniai/config.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module OmniAI
+  # A configuration for each agent w/ `api_key` / `host` / `logger`.
+  class Config
+    attr_accessor :api_key, :host, :logger
+  end
+end
