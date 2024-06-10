562b5a6e3454aef4dfea8c594e1a53b35adfc66c
Kevin Sylvestre <kevin@ksylvest.com>
2024-06-10 12:12:14 -0700

    Setup

---
+++ spec/omniai/client_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+RSpec.describe OmniAI::Client do
+  subject(:client) { described_class.new(api_key:) }
+
+  let(:api_key) { '...' }
+
+  describe '#api_key' do
+    it { expect(client.api_key).to eq(api_key) }
+  end
+
+  describe '#connection' do
+    it { expect { client.connection }.to raise_error(NotImplementedError) }
+  end
+
+  describe '#chat' do
+    it { expect { client.chat }.to raise_error(NotImplementedError) }
+  end
+end
