cac55c9c37f2333289e422661039de3433a8496c
Kevin Sylvestre <kevin@ksylvest.com>
2024-06-17 11:30:22 -0700

    Better define OmniAI::Chat::Message
    This avoids using the generic data for message.

--- lib/omniai/chat/message.rb
+++ lib/omniai/chat/message.rb
@@ -4,22 +4,27 @@ module OmniAI
   class Chat
     # A message returned by the API.
     class Message
-      attr_accessor :data
+      attr_accessor :role, :content
 
-      # @param content [Integer]
-      # @param role [String]
-      def initialize(data:)
-        @data = data
+      # @param data [Hash]
+      # @return [OmniAI::Chat::Message]
+      def self.for(data:)
+        content = data['content'] || data[:content]
+        role = data['role'] || data[:role]
+
+        new(content:, role: role || Role::USER)
+      end
+
+      # @param content [String]
+      # @param role [String] optional (default to "user") e.g. "assistant" / "user" / "system"
+      def initialize(content:, role: OmniAI::Chat::Role::USER)
+        @role = role
+        @content = content
       end
 
       # @return [String]
-      def role
-        @data['role']
-      end
-
-      # @return [String]
-      def content
-        @data['content']
+      def inspect
+        "#<#{self.class.name} role=#{role.inspect} content=#{content.inspect}>"
       end
     end
   end
