219a7929ac046106bd0820ed8f989ec891b9ba22
Kevin Sylvestre <kevin@ksylvest.com>
2024-06-17 12:46:00 -0700

    Test mask for inspect on client

--- spec/omniai/client_spec.rb
+++ spec/omniai/client_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe OmniAI::Client do
   subject(:client) { described_class.new(api_key:) }
 
-  let(:api_key) { '...' }
+  let(:api_key) { 'abcdef' }
 
   describe '#api_key' do
     it { expect(client.api_key).to eq(api_key) }
@@ -15,5 +15,9 @@ RSpec.describe OmniAI::Client do
 
   describe '#chat' do
     it { expect { client.chat('Hello!', model: '...') }.to raise_error(NotImplementedError) }
+  end
+
+  describe '#inspect' do
+    it { expect(client.inspect).to eq('#<OmniAI::Client api_key="abc***">') }
   end
 end
