c7f58c179365b5190c5e27f2c0c617d7f4279cd2
Kevin Sylvestre <kevin@ksylvest.com>
2024-06-20 13:18:25 -0700

    Better support for inspect on client
    This includes the host to make it easier to inspect clients.

--- lib/omniai/client.rb
+++ lib/omniai/client.rb
@@ -13,19 +13,28 @@ module OmniAI
   class Client
     class Error < StandardError; end
 
-    attr_accessor :api_key
+    attr_accessor :api_key, :logger, :host
 
-    # @param api_key [String]
-    # @param logger [Logger]
-    def initialize(api_key:, logger: nil)
+    # @param api_key [String] optional
+    # @param host [String] optional - supports for customzing the host of the client (e.g. 'http://localhost:8080')
+    # @param logger [Logger] optional
+    def initialize(api_key: nil, logger: nil, host: nil)
       @api_key = api_key
+      @host = host
       @logger = logger
     end
 
     # @return [String]
     def inspect
-      masked_api_key = "#{api_key[..2]}***" if api_key
-      "#<#{self.class.name} api_key=#{masked_api_key.inspect}>"
+      props = []
+      props << "api_key=#{masked_api_key.inspect}" if @api_key
+      props << "host=#{@host.inspect}" if @host
+      "#<#{self.class.name} #{props.join(' ')}>"
+    end
+
+    # @return [String]
+    def masked_api_key
+      "#{api_key[..2]}***" if api_key
     end
 
     # @return [HTTP::Client]
--- spec/omniai/client_spec.rb
+++ spec/omniai/client_spec.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 RSpec.describe OmniAI::Client do
-  subject(:client) { described_class.new(api_key:) }
+  subject(:client) { described_class.new(api_key:, host:) }
 
   let(:api_key) { 'abcdef' }
+  let(:host) { 'http://localhost:8080' }
 
   describe '#api_key' do
     it { expect(client.api_key).to eq(api_key) }
+  end
+
+  describe '#host' do
+    it { expect(client.host).to eq(host) }
   end
 
   describe '#connection' do
@@ -18,6 +23,6 @@ RSpec.describe OmniAI::Client do
   end
 
   describe '#inspect' do
-    it { expect(client.inspect).to eq('#<OmniAI::Client api_key="abc***">') }
+    it { expect(client.inspect).to eq('#<OmniAI::Client api_key="abc***" host="http://localhost:8080">') }
   end
 end
