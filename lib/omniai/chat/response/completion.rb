dc791e10ee24d3273a25a3a4c526211b3055e8fb
Kevin Sylvestre <kevin@ksylvest.com>
2024-07-12 11:50:12 -0700

    Move tool_call_required / tool_call_list

--- lib/omniai/chat/response/completion.rb
+++ lib/omniai/chat/response/completion.rb
@@ -9,20 +9,6 @@ module OmniAI
         def choices
           @choices ||= @data['choices'].map { |data| MessageChoice.new(data:) }
         end
-
-        # @return [Boolean]
-        def tool_call_required?
-          choices.any? { |choice| choice.message.tool_call_list.any? }
-        end
-
-        # @return [Array<OmniAI::Chat::Response:ToolCall>]
-        def tool_call_list
-          list = []
-          choices.each do |choice|
-            list += choice.message.tool_call_list
-          end
-          list
-        end
       end
     end
   end
