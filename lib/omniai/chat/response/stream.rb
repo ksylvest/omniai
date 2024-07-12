6c931e946ee723d05c4641f3a688ec2acfbd283f
Kevin Sylvestre <kevin@ksylvest.com>
2024-07-12 11:34:44 -0700

    Move Stream under Response namespace

---
+++ lib/omniai/chat/response/stream.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module OmniAI
+  class Chat
+    module Response
+      # A stream given when streaming.
+      class Stream
+        # @param response [HTTP::Response]
+        def initialize(response:)
+          @response = response
+          @parser = EventStreamParser::Parser.new
+        end
+
+        # @yield [OmniAI::Chat::Chunk]
+        def stream!
+          @response.body.each do |chunk|
+            @parser.feed(chunk) do |_, data|
+              next if data.eql?('[DONE]')
+
+              yield(Chunk.new(data: JSON.parse(data)))
+            end
+          end
+        end
+      end
+    end
+  end
+end
