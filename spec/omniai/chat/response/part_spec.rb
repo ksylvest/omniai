5f8a304213c9183c0f2328533084821162062e2d
Kevin Sylvestre <kevin@ksylvest.com>
2024-07-12 11:53:58 -0700

    Properly cover tool_call

--- spec/omniai/chat/response/part_spec.rb
+++ spec/omniai/chat/response/part_spec.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
 RSpec.describe OmniAI::Chat::Response::Part do
-  subject(:part) { described_class.new(data: { 'role' => 'system', 'content' => 'Hello!' }) }
+  subject(:part) { described_class.new(data:) }
+
+  let(:data) do
+    {
+      'role' => 'system',
+      'content' => 'Hello!',
+      'tool_calls' => [
+        {
+          'id' => 'fake_tool_call_id',
+          'type' => 'function',
+          'function' => {
+            'name' => 'temperature',
+            'arguments' => '{"unit":"celsius"}',
+          },
+        },
+      ],
+    }
+  end
 
   describe '#role' do
     it { expect(part.role).to eq('system') }
@@ -9,5 +26,13 @@ RSpec.describe OmniAI::Chat::Response::Part do
 
   describe '#content' do
     it { expect(part.content).to eq('Hello!') }
+  end
+
+  describe '#tool_call_list' do
+    it { expect(part.tool_call_list).not_to be_empty }
+  end
+
+  describe '#tool_call' do
+    it { expect(part.tool_call).to be_a(OmniAI::Chat::Response::ToolCall) }
   end
 end
