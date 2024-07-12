02a85f24bf4c8c0d7e690688b3d7a13759c32d8f
Kevin Sylvestre <kevin@ksylvest.com>
2024-07-12 10:26:22 -0700

    Combine Message and Delta into Part

--- lib/omniai/chat/delta.rb
+++ lib/omniai/chat/delta.rb
@@ -3,26 +3,7 @@
 module OmniAI
   class Chat
     # A delta returned by the API.
-    class Delta
-      # @param data [Hash]
-      def initialize(data:)
-        @data = data
-      end
-
-      # @return [String]
-      def inspect
-        "#<#{self.class.name} role=#{role.inspect} content=#{content.inspect}>"
-      end
-
-      # @return [String, nil]
-      def content
-        @data['content']
-      end
-
-      # @return [String]
-      def role
-        @data['role'] || Role::USER
-      end
+    class Delta < OmniAI::Chat::Part
     end
   end
 end
