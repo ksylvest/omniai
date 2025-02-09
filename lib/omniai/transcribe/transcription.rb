ddbbad1c5726fe87f3b6fbe2298878f56ccfc88e
Kevin Sylvestre <kevin@ksylvest.com>
2025-02-09 08:59:31 -0800

    Fix attributes w/o documentation

--- lib/omniai/transcribe/transcription.rb
+++ lib/omniai/transcribe/transcription.rb
@@ -4,7 +4,17 @@ module OmniAI
   class Transcribe
     # A transcription returned by the API.
     class Transcription
-      attr_accessor :text, :model, :format
+      # @!attribute [rw] text
+      #   @return [String]
+      attr_accessor :text
+
+      # @!attribute [rw] model
+      #   @return [String]
+      attr_accessor :model
+
+      # @!attribute [rw] format
+      #   @return [String]
+      attr_accessor :format
 
       # @param text [String]
       # @param model [String]
