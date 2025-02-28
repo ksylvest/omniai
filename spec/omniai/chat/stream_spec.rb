c206b88da0cb764d818a7325fc46bdfb135df2a7
Kevin Sylvestre <kevin@ksylvest.com>
2025-02-28 12:44:38 -0800

    Cleanup stream spec

--- spec/omniai/chat/stream_spec.rb
+++ spec/omniai/chat/stream_spec.rb
@@ -8,23 +8,22 @@ RSpec.describe OmniAI::Chat::Stream do
 
     let(:chunks) do
       [
-        "data: #{JSON.generate({
-          id: 'chunk',
-          object: 'chat.completion.chunk',
-          choices: [{ index: 0, delta: { role: 'assistant', content: '' } }],
-        })}\n\n",
-        "data: #{JSON.generate({
-          id: 'chunk',
-          object: 'chat.completion.chunk',
-          choices: [{ index: 0, delta: { content: 'A' } }],
-        })}\n\n",
-        "data: #{JSON.generate({
-          id: 'chunk',
-          object: 'chat.completion.chunk',
-          choices: [{ index: 0, delta: { content: 'B' } }],
-        })}\n\n",
-        "data: [DONE]\n",
-      ]
+        {
+          id: "chunk",
+          object: "chat.completion.chunk",
+          choices: [{ index: 0, delta: { role: "assistant", content: "" } }],
+        },
+        {
+          id: "chunk",
+          object: "chat.completion.chunk",
+          choices: [{ index: 0, delta: { content: "A" } }],
+        },
+        {
+          id: "chunk",
+          object: "chat.completion.chunk",
+          choices: [{ index: 0, delta: { content: "B" } }],
+        },
+      ].map { |chunk| "data: #{JSON.generate(chunk)}\n\n" }
     end
 
     it { expect(stream!).to be_a(OmniAI::Chat::Payload) }
