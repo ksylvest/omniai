0527d3f24512a095e6ddcacb2324883c775a33ab
Kevin Sylvestre <kevin@ksylvest.com>
2025-03-21 11:50:40 -0700

    Helpers for chat / transcribe / speak / embed
    This by default uses a discovered client

--- lib/omniai.rb
+++ lib/omniai.rb
@@ -16,6 +16,32 @@ loader.inflector.inflect "http_error" => "HTTPError"
 loader.inflector.inflect "ssl_error" => "SSLError"
 loader.setup
 
+# @example
+#
+#   OmniAI.chat(...)
+#   OmniAI.transcribe(...)
+#   OmniAI.speak(...)
+#   OmniAI.embed(...)
 module OmniAI
   class Error < StandardError; end
+
+  # @see OmniAI::Client#chat
+  def self.chat(...)
+    OmniAI::Client.discover.chat(...)
+  end
+
+  # @see OmniAI::Client#transcribe
+  def self.transcribe(...)
+    OmniAI::Client.discover.transcribe(...)
+  end
+
+  # @see OmniAI::Client#speak
+  def self.speak(...)
+    OmniAI::Client.discover.speak(...)
+  end
+
+  # @see OmniAI::Client#embed
+  def self.embed(...)
+    OmniAI::Client.discover.embed(...)
+  end
 end
