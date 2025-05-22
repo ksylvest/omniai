4a2074b5996c7d201a37ff9a17b0c3adf65aafa1
Kevin Sylvestre <kevin@ksylvest.com>
2025-05-22 10:20:50 -0700

    Support a default format for speak
    Opting for AAC since it appears to be fairly well supported.

--- lib/omniai/speak.rb
+++ lib/omniai/speak.rb
@@ -34,6 +34,8 @@ module OmniAI
       WAV = "wav"
     end
 
+    DEFAULT_FORMAT = Format::AAC
+
     # @raise [HTTPError]
     #
     # @param client [OmniAI::Client] required
@@ -52,7 +54,7 @@ module OmniAI
     # @yield [chunk]
     #
     # @return [Tempfile]
-    def self.process!(input, client:, model:, voice:, speed: nil, format: nil, &)
+    def self.process!(input, client:, model:, voice:, speed: nil, format: DEFAULT_FORMAT, &)
       new(input, client:, model:, voice:, speed:, format:).process!(&)
     end
 
@@ -68,7 +70,7 @@ module OmniAI
     #   - "opus"
     #   - "pcm"
     #   - "wav"
-    def initialize(input, client:, model:, voice:, speed: nil, format: nil)
+    def initialize(input, client:, model:, voice:, speed: nil, format: DEFAULT_FORMAT)
       @input = input
       @client = client
       @model = model
